34050d745861d1efa1b4891ce8bdfde84d1ed5ec
Uvindu Kavishka <kavishkauvindu0@gmail.com>
2023-05-06 21:41:12 +0530

    2nd Commit

--- app/src/main/java/com/example/influxapp/AchievmentActivity.kt
+++ app/src/main/java/com/example/influxapp/AchievmentActivity.kt
@@ -1,11 +1,22 @@
 package com.example.influxapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class AchievmentActivity : AppCompatActivity() {
+    private lateinit var backBtn:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_achievment)
+
+        backBtn = findViewById(R.id.achievementBackBtn)
+
+        backBtn.setOnClickListener {
+            val intent = Intent(this,DashboardActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
--- app/src/main/java/com/example/influxapp/DashboardActivity.kt
+++ app/src/main/java/com/example/influxapp/DashboardActivity.kt
@@ -1,7 +1,9 @@
 package com.example.influxapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import com.example.influxapp.database.models.ConfidentialDataClass
 import com.example.influxapp.database.models.ConfidentialDataModel
@@ -9,12 +11,20 @@ import com.example.influxapp.database.models.ConfidentialDataModel
 class DashboardActivity : AppCompatActivity() {
 
     private lateinit var cardNumberPrev:TextView
+    private lateinit var achievementBtn: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
         val confiData = ConfidentialDataModel()
         cardNumberPrev = findViewById(R.id.cardNumberPrev)
         cardNumberPrev.text = confiData.cardNumber
+        achievementBtn = findViewById(R.id.achievementBtn)
+
+        achievementBtn.setOnClickListener {
+            val intent = Intent(this,AchievmentActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
